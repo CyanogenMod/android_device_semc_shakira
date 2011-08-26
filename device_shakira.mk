0e84e799f38eec68867c17bd085a37422732afae
jerpelea <jerpelea@gmail.com>
2011-08-26 06:20:56 +0300

    sync

--- device_shakira.mk
+++ device_shakira.mk
@@ -33,7 +33,8 @@ DEVICE_PACKAGE_OVERLAYS += device/semc/shakira/overlay
 # These are the hardware-specific configuration files
 PRODUCT_COPY_FILES += \
 	device/semc/shakira/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
-	device/semc/msm7x27-common/prebuilt/initlogo_mdpi.rle:root/initlogo.rle 
+	device/semc/msm7x27-common/prebuilt/initlogo_mdpi.rle:root/initlogo.rle \
+    device/semc/shakira/prebuilt/AudioFilter.csv:system/etc/AudioFilter.csv
 
 # Init files
 PRODUCT_COPY_FILES += \
