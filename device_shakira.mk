74a2300a62346d2a7584e8ab78eb9cc48aa4f2ac
jerpelea <jerpelea@gmail.com>
2011-09-24 11:40:08 +0300

    shakira : device sync and prebuilt kernel.sin

--- device_shakira.mk
+++ device_shakira.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/semc/shakira/prebuilt/bootrec:root/sbin/bootrec 
 
+# kernel.sin prebuilt for now
+PRODUCT_COPY_FILES += \
+    device/semc/shakira/prebuilt/kernel.sin:system/kernel.sin
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
 
