b6a8228d07f88b4cb09735b9bbde6da0c351b8e3
Diogo Ferreira <defer@cyanogenmod.com>
2011-09-28 22:18:25 +0100

    Shakira: Copy kernel to the correct folder
    Change-Id: I2b4348780d918846f92bec0e7e4819d584d3cb1c

--- device_shakira.mk
+++ device_shakira.mk
@@ -44,7 +44,7 @@ PRODUCT_COPY_FILES += \
 
 # kernel.sin prebuilt for now
 PRODUCT_COPY_FILES += \
-    device/semc/shakira/prebuilt/kernel.sin:system/kernel.sin
+    device/semc/shakira/prebuilt/kernel.sin:kernel.sin
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160 \
