9b11bba7b7fda96f24839a1e60dcba1f8c4c0692
jerpelea <jerpelea@gmail.com>
2011-10-06 12:34:52 +0300

    shakira:added suport for W8 devices (same hardware as X8)
    Change-Id: Ie455a81227f6eb5efb8b36094e0e4ebaca6be585

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,6 @@
 TARGET_BOOTLOADER_BOARD_NAME := shakira
 TARGET_SPECIFIC_HEADER_PATH := device/semc/shakira/include
 
-TARGET_OTA_ASSERT_DEVICE := E15i,E15a,shakira
+TARGET_OTA_ASSERT_DEVICE := E15i,E15a,E16i,E16a,shakira
 
 -include device/semc/msm7x27-common/Android.mk
